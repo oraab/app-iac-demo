120daa310f29a97e2ee4b7e2eba5a73494cfb87c
Oren Raab <oraab@orens-mbp.mynet>
2020-03-21 23:25:47 +0200

    adding domain_name as variable to live TF configurations

--- terraform/live/staging/application/variables.tf
+++ terraform/live/staging/application/variables.tf
@@ -19,3 +19,9 @@ variable "image_tag" {
 	description = "The tag name for the image that should be pulled for the application"
 	type = string
 }
+
+variable "domain_name" {
+	description = "The registered domain name for which a certificate should be uploaded to the ALB for the application."
+	type = string 
+	default = "testing-placeholder.xyz"
+}
