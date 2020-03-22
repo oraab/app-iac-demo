739ed2af7ab7d99d8fdca96d5bcaf68ed2cec504
Oren Raab <oraab@orens-mbp.mynet>
2020-03-22 04:04:00 +0200

    adding egress rule to security group module

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -58,3 +58,12 @@ resource "aws_security_group_rule" "alb_external_inbound_https" {
 	cidr_blocks = ["0.0.0.0/0"]
 	security_group_id = aws_security_group.alb.id
 }
+
+resource "aws_security_group_rule" "alb_egress" {
+	type = "egress"
+	from_port = 0
+	to_port = 0
+	protocol = "-1"
+	cidr_blocks = ["0.0.0.0/0"]
+	security_group_id = aws_security_group.alb.id
+}
