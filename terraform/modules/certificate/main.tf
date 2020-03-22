672a8e3a072db9c97983ae3dc1867cea30689abe
Oren Raab <oraab@orens-mbp.mynet>
2020-03-22 07:02:11 +0200

    fixing issue with domain_name in certificate

--- terraform/modules/certificate/main.tf
+++ terraform/modules/certificate/main.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "alb_default_cert" {
 	domain_name = "${var.domain_name}" 
-	subject_alternative_names = ["*.${domain_name}"]
+	subject_alternative_names = ["*.${var.domain_name}"]
 	validation_method = "DNS"
 
 	tags = {
